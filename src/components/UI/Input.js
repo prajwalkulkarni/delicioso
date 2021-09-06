94bd5dbf556a3d31b1266e232bd06196b3766ca0
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-06 15:11:02 +0530

    Input component with passed arg props

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import classes from './Input.module.css'
+export default function Input(props){
+    //alert(props.activeclass)
+    const activeClass = props.activeclass
+    return(
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}
+            </label>
+
+            <input {...props.input} className={activeClass==="error"?classes.error:classes.normal}/>
+        </div>
+    )
+}
