aadf1e81a17985ea2c928936aca8917ff5eab5a2
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-07 23:26:13 +0530

    Card view food item

--- src/components/Foods/FoodItem.js
+++ src/components/Foods/FoodItem.js
@@ -8,15 +8,21 @@ export default function FoodItem(props){
     return(
         
         <div key={props.id} className={classes.foodHolder}>
-            <div className={classes.nameDesc}>
-                <b>{props.name}</b>
-                {props.desc}
-               <b className={classes.price}>${props.price}</b>
-            </div>
-
-            <div>
+            <div className={classes.foodItem}>
+                <img src={props.pic} alt="food" height="180" width="180" style={{borderRadius:15}} />
+                <div className={classes.nameDesc}>
+                    <b>{props.name}</b>
+                    {props.desc}
+                    <b className={classes.price}>${props.price}</b>
+                </div>
+                <div>
                 <FoodItemForm id={props.id} price={props.price} name={props.name}/>
             </div>
+
+            </div>
+            
+
+            
 
         </div>
     )
