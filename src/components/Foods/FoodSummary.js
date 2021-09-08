1c8582784dcec515cb8e3ec2c3c574a3cc239181
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-08 23:08:40 +0530

    Edit summary

--- src/components/Foods/FoodSummary.js
+++ src/components/Foods/FoodSummary.js
@@ -2,7 +2,7 @@ import classes from './FoodSummary.module.css'
 export default function FoodSummary(){
     return(
         <section className={classes.summary}>
-        <h2>Delicious Food recommendation</h2>
+        <h2>Delicious Food, just in time!</h2>
         <p>
           Choose your favorite meal from our broad selection of available meals
           and enjoy a delicious lunch or dinner at home.
