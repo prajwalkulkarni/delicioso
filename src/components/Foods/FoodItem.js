37b97fa6346b0eec6b405a6099be07ed4a0097f7
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-13 20:15:31 +0530

    set progress bar while image loads

--- src/components/Foods/FoodItem.js
+++ src/components/Foods/FoodItem.js
@@ -1,15 +1,26 @@
 
 import classes from './FoodItem.module.css'
 import FoodItemForm from './FoodItemForm'
+import { CircularProgress } from '@material-ui/core'
+import { useState } from 'react'
 export default function FoodItem(props){
 
-    
+    const [load,setLoad] = useState(false)
+
+    function toggleSpinner(){
+        console.log("called")
+        setLoad(true)
+
+    }
+    const imgClassName = load? classes.img: classes.img+classes.nodisplay
 
     return(
         
         <div key={props.id} className={classes.foodHolder}>
             <div className={classes.foodItem}>
-                <img src={props.pic} alt="food" height="180" width="180" style={{borderRadius:15}} />
+                {!load&&<CircularProgress/>}
+                <img src={props.pic} alt="food" height="180" width="180" className={imgClassName} onLoad ={toggleSpinner} />
+                
                 <div className={classes.nameDesc}>
                     <b>{props.name}</b>
                     {props.desc}
